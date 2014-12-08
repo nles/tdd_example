79bda4c132d97fa2c0cd2481a5d8633d4794eb04
Nikke <nileskel@gmail.com>
2014-12-08 18:30:51 +0200

    step7

--- person_model.js
+++ person_model.js
@@ -5,4 +5,12 @@ var Person = function(name,height,weight){
   this.weight = weight
 }
 
+Person.prototype.calculateBMI = function(){
+  var heightInMeters = this.height/100
+  var bmi = this.weight / (heightInMeters * heightInMeters);
+  // round to two decimal places
+  this.bmi = Math.round(bmi * 100) / 100
+  return this.bmi;
+}
+
 module.exports = Person;
